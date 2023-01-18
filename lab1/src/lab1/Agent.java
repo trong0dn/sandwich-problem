0c2f47b43216d2cfc67a0064272919157e4b5566
trong0dn <55768917+trong0dn@users.noreply.github.com>
2023-01-18 02:07:37 -0500

    Removed unused import

--- lab1/src/lab1/Agent.java
+++ lab1/src/lab1/Agent.java
@@ -1,7 +1,6 @@
 package lab1;
 
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Agent class represents an agent that randomly chooses two ingredients to be placed on the table.
